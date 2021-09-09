0b764cb6d8d772f94d31c5aaf3f391995706dbb9
Danny <ctct25@naver.com>
2021-09-09 14:44:05 +0900

    210909 14:26 PARTY LIST by 대근

--- src/main/java/com/nextroom/controller/Hello.java
+++ src/main/java/com/nextroom/controller/Hello.java
@@ -22,6 +22,6 @@ public class Hello {
    @RequestMapping("/party")
    public String party() {
       System.out.println("hello");
-      return "user/userrank";
+      return "party_list";
    }
 }
