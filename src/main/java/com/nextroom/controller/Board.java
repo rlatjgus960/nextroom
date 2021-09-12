b02f82adde6fb37016b7b1913e1628276aea0466
wonho <wonho951@gmail.com>
2021-09-12 22:08:03 +0900

    2021.09.12 커뮤니티(하는중 css 왜케 안먹어ㅡㅡ)최원호

--- src/main/java/com/nextroom/controller/Board.java
+++ src/main/java/com/nextroom/controller/Board.java
@@ -11,5 +11,11 @@ public class Board {
 		System.out.println("board");
 		return "board/freeCommunity";
 	}
+	
+	@RequestMapping("/community_read")
+	public String read() {
+		System.out.println("read");
+		return "board/read";
+	}
 
 }
