c093e821424c7ea95da4e67467014e3363bc5970
junzzan9 <ojs2681@gmail.com>
2021-09-13 13:54:24 +0900

    09/13 hello 수정

--- src/main/java/com/nextroom/controller/Hello.java
+++ src/main/java/com/nextroom/controller/Hello.java
@@ -6,60 +6,70 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Hello {
 
-   @RequestMapping("/hello")
-   public String hello() {
-      System.out.println("hello");
-      return "user/mypage_main";
-   }
-   
-   
-   @RequestMapping("/rankUser")
-   public String rankUser() {
-      System.out.println("hello");
-      return "rank/rank_user";
-   }
-   
-   @RequestMapping("/rankTheme")
-   public String rankTheme() {
-      System.out.println("hello");
-      return "rank/rank_theme";
-   }
+	@RequestMapping("/hello")
+	public String hello() {
+		System.out.println("hello");
+		return "user/mypage_main";
+	}
 
-   @RequestMapping("/party")
-   public String party() {
-      System.out.println("hello");
-      return "party/party_list";
-   }
-   
-   @RequestMapping("/partyRead")
-   public String partyRead() {
-      System.out.println("hello");
-      return "party/party_read";
-   }
-   
-   
-   @RequestMapping("/reserveBaseForm")
-   public String reserveBaseForm() {
-      System.out.println("reserveBaseForm");
-      return "reserve/reserveBaseForm";
-   }
-   
-   @RequestMapping("/reserveInfoForm")
-   public String reserveInfoForm() {
-      System.out.println("reserveInfoForm");
-      return "reserve/reserveInfoForm";
-   }
-   
-   @RequestMapping("/reservePayForm")
-   public String reservePayForm() {
-      System.out.println("reservePayForm");
-      return "reserve/reservePayForm";
-   }
-   
-   @RequestMapping("/reserveComplete")
-   public String reserveComplete() {
-      System.out.println("reserveComplete");
-      return "reserve/reserveComplete";
-   }
-   
+	///////// 유저랭킹///////////
+	@RequestMapping("/rankUser")
+	public String rankUser() {
+		System.out.println("hello");
+		return "rank/rank_user";
+	}
+
+	//////// 테마랭킹////////////
+	@RequestMapping("/rankTheme")
+	public String rankTheme() {
+		System.out.println("hello");
+		return "rank/rank_theme";
+	}
+
+	////////개인상세////////////
+	@RequestMapping("/userProfile")
+	public String userProfile() {
+		System.out.println("hello");
+		return "rank/user_detail";
+	}
+	////////파티 리스트//////////
+	@RequestMapping("/party")
+	public String party() {
+		System.out.println("hello");
+		return "party/party_list";
+	}
+	
+	////////파티 읽기폼//////////
+	@RequestMapping("/partyRead")
+	public String partyRead() {
+		System.out.println("hello");
+		return "party/party_read";
+	}
+	////////예약 기본폼//////////
+	@RequestMapping("/reserveBaseForm")
+	public String reserveBaseForm() {
+		System.out.println("reserveBaseForm");
+		return "reserve/reserveBaseForm";
+	}
+	
+	////////예약 정보폼//////////
+	@RequestMapping("/reserveInfoForm")
+	public String reserveInfoForm() {
+		System.out.println("reserveInfoForm");
+		return "reserve/reserveInfoForm";
+	}
+	
+	////////예약 결제폼//////////
+	@RequestMapping("/reservePayForm")
+	public String reservePayForm() {
+		System.out.println("reservePayForm");
+		return "reserve/reservePayForm";
+	}
+	////////예약 완료폼//////////
+	@RequestMapping("/reserveComplete")
+	public String reserveComplete() {
+		System.out.println("reserveComplete");
+		return "reserve/reserveComplete";
+	}
+
 }
