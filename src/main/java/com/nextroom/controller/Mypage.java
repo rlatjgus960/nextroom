18b49e25c4825f88d38878e47637ba4cbc89a8d1
rlatjgus960 <rlatjgus960@naver.com>
2021-09-24 18:00:52 +0900

    210924 카페관리자전환 수정중

--- src/main/java/com/nextroom/controller/Mypage.java
+++ src/main/java/com/nextroom/controller/Mypage.java
@@ -6,50 +6,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/mypage")
 public class Mypage {
-	
-	
-	
-	//마이페이지 메인
+
+	// 마이페이지 메인
 	@RequestMapping("/main")
-	public String main(){
+	public String main() {
 		System.out.println("mypage/main");
 		return "mypage/main";
 	}
-	
-	//마이페이지 개인정보 수정
+
+	// 마이페이지 개인정보 수정
 	@RequestMapping("/infoModifyForm")
 	public String infoModifyForm() {
 		System.out.println("mypage/infoModifyForm");
 		return "mypage/infoModifyForm";
 	}
-	
-	
-	//마이페이지 받은메시지
+
+	// 마이페이지 받은메시지
 	@RequestMapping("/receiveMassage")
 	public String receiveMassage() {
 		System.out.println("mypage/receiveMassage");
 		return "mypage/receiveMassage";
 	}
-	
-	//마이페이지 파티관리
+
+	// 마이페이지 파티관리
 	@RequestMapping("/partyManage")
 	public String mypageParty() {
 		System.out.println("mypage/partyManage");
 		return "mypage/partyManage";
 	}
-	
-	//마이페이지 결제내역
+
+	// 마이페이지 결제내역
 	@RequestMapping("/payMent")
 	public String payMent() {
 		System.out.println("mypage/payMent");
 		return "mypage/payMent";
 	}
-	
-	//마이페이지 보낸메시지
+
+	// 마이페이지 보낸메시지
 	@RequestMapping("/sendMassage")
 	public String sendMassage() {
 		System.out.println("mypage/sendMassage");
 		return "mypage/sendMassage";
 	}
-	
+
+	// 마이페이지 카페관리자 전환
+	@RequestMapping("/addCafeForm")
+	public String addCafeForm() {
+		System.out.println("mypage/addCafeForm");
+		return "mypage/addCafeForm";
+	}
+
 }
