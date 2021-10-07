b6ba7d5a8a899a8131aa770887236c2594f637f0
rlatjgus960 <rlatjgus960@naver.com>
2021-10-08 00:56:16 +0900

    V.O.S - 제자리 걸음

--- src/main/java/com/nextroom/controller/Cafe.java
+++ src/main/java/com/nextroom/controller/Cafe.java
@@ -15,17 +15,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nextroom.service.CafeService;
 import com.nextroom.vo.CafeVo;
-import com.nextroom.vo.PriceVo;
 
 @RequestMapping("/cafe")
 @Controller
 public class Cafe {
-	
+
 	@Autowired
 	private CafeService cafeService;
-	
 
-	//카페 메인
+
+	// 카페 메인
 	@RequestMapping(value="", method = { RequestMethod.GET, RequestMethod.POST })
 	public String cafeMain(HttpSession session, Model model, 
 						   @RequestParam(value="region", required = false, defaultValue = "전국") String region) {
@@ -37,47 +36,43 @@ public class Cafe {
 	   
 	   return "cafe/cafeMain";
 	}
-	
-	//카페 상세페이지
-	@RequestMapping(value="/{cafeNo}", method = { RequestMethod.GET, RequestMethod.POST })
+
+	// 카페 상세페이지
+	@RequestMapping(value = "/{cafeNo}", method = { RequestMethod.GET, RequestMethod.POST })
 	public String cafeDetail(@PathVariable("cafeNo") int cafeNo, Model model) {
-	   System.out.println("cafeDetail");
-	   
-	   
-	   Map<Object, Object> detailMap = cafeService.getCafeDetail(cafeNo);
-	   
-	   
-	   model.addAttribute("detailMap", detailMap);
-	   
-	   
-	   return "cafe/cafeDetail";
+		System.out.println("cafeDetail");
+
+		Map<Object, Object> detailMap = cafeService.getCafeDetail(cafeNo);
+
+		model.addAttribute("detailMap", detailMap);
+
+		return "cafe/cafeDetail";
 	}
-	
-	//테마 메인
-	@RequestMapping(value="/theme", method = { RequestMethod.GET, RequestMethod.POST })
+
+	// 테마 메인
+	@RequestMapping(value = "/theme", method = { RequestMethod.GET, RequestMethod.POST })
 	public String themeMain(Model model) {
-	   System.out.println("themeMain");
-	   
-	   model.addAttribute("themeList", cafeService.getThemeList());
-	   
-	   return "theme/themeMain";
+		System.out.println("themeMain");
+
+		model.addAttribute("themeList", cafeService.getThemeList());
+
+		return "theme/themeMain";
 	}
-	
-	//테마 상세페이지
-	@RequestMapping(value="/theme/{themeNo}", method = { RequestMethod.GET, RequestMethod.POST })
+
+	// 테마 상세페이지
+	@RequestMapping(value = "/theme/{themeNo}", method = { RequestMethod.GET, RequestMethod.POST })
 	public String themeDetail(@PathVariable("themeNo") int themeNo, Model model) {
-	   System.out.println("themeDetail");
-	   
+		System.out.println("themeDetail");
+
 //	   Map<Object, Object> themeMap = cafeService.getTheme(themeNo);
 //	   model.addAttribute("themeMap", themeMap);
-	   
-	   model.addAttribute("themeVo", cafeService.getOneTheme(themeNo));
-	   model.addAttribute("timeList", cafeService.getOneTime(themeNo));
-	   model.addAttribute("priceList", cafeService.getOnePrice(themeNo));
-	   model.addAttribute("reviewList", cafeService.getReviewList(themeNo));
-	   
-	   return "theme/themeDetail";
+
+		model.addAttribute("themeVo", cafeService.getOneTheme(themeNo));
+		model.addAttribute("timeList", cafeService.getOneTime(themeNo));
+		model.addAttribute("priceList", cafeService.getOnePrice(themeNo));
+		model.addAttribute("reviewList", cafeService.getReviewList(themeNo));
+
+		return "theme/themeDetail";
 	}
-	
-	
+
 }
